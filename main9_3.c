32c373a619be6cf3c77fce0df301f30bf97db6eb
Pavel Priklonskiy <votymu@yandex.ru>
2023-11-08 13:07:50 +0300

    Add output9_3.txt update data9_3.txt main9_3.c

--- main9_3.c
+++ main9_3.c
@@ -1,32 +1,84 @@
-/*
- * main9_3.c
- * 
- * Copyright 2023 night <night@LT-NH>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
-
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argc, char **argv)
+const int size = 5;
+
+void init_arr(int arr[])
 {
-	
-	return 0;
+	for (int i = 0; i < size; i++)
+		arr[i] = 0;
 }
 
+void read_file(char *str, char *filename_read)
+{
+	FILE *fp;
+	char ch;
+	int i = 0;
+	fp = fopen(filename_read, "r");
+	while((ch = fgetc(fp)) != EOF)
+		str[i++] = ch;
+	str[i] = '\0';
+	fclose(fp);
+}
+
+void set_arr(char *str, int arr[])
+{
+	int coun = 0;
+	init_arr(arr);
+	for (int j = 0; j <strlen(str); j++)
+	{
+		if (coun > size)
+		{
+			printf("Error! Please, increase the parameter: size!");
+			break;
+		}
+		if (isdigit(str[j])){
+			arr[coun] = arr[coun] * 10 + str[j] - '0';
+		}
+		if (isdigit(str[j]) && !(isdigit(str[j+1])))
+			coun++;
+	}
+}
+void write_file(int arr[], char *filename_write)
+{
+	FILE *fp = fopen(filename_write, "w");
+	if(fp)
+    {
+		for (int i = 0; i < size; i++)
+		{
+			fprintf(fp, "%d ", arr[i]);
+		}
+		fclose(fp);
+	}
+}
+
+void sort_array(int size, int a[])
+{
+    int tmp;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            tmp = a[j];
+            if (tmp > a[j + 1])
+            {
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+}
+
+int main(void)
+{
+	char str[1000] = {0};
+	int arr[size];
+    char * filename_read = "data9_3.txt";
+    char * filename_write = "output9_3.txt";
+    read_file(str, filename_read);
+    set_arr(str, arr);
+    sort_array(size, arr);
+    write_file(arr, filename_write);
+    return 0;
+}
